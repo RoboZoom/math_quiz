1b2cd098d2d17d84bcf6f5447b35c33c1089f488
Brian Sump <brian.sump@gmail.com>
2025-09-23 20:39:35 -0600

    Initialized quiz cache events.

--- lib/math_quiz/quiz_cache.ex
+++ lib/math_quiz/quiz_cache.ex
@@ -11,4 +11,22 @@ defmodule MathQuiz.QuizCache do
        quizzes: []
      }}
   end
+
+  @doc """
+  Returns {:ok, Quiz} or {:error, _error}
+  """
+  @impl true
+  def handle_call({:get_quiz, quiz_id}, _from, %Models.QuizCache{} = state) do
+    quiz = Enum.find(state.quizzes, &(&1.id == quiz_id))
+
+    case quiz do
+      nil -> {:reply, {:error, "Quiz not found"}, state}
+      _ -> {:reply, {:ok, quiz}, state}
+    end
+  end
+
+  @impl true
+  def handle_cast({:generate_quiz, params}, _from, %Models.QuizCache{} = state) do
+    # To Implement
+  end
 end
