7745e00081be4460d9d608323ca3f8fb67185a56
RoboZoom <56803986+RoboZoom@users.noreply.github.com>
2025-10-02 19:04:42 -0600

    Included subtraction problems.

--- lib/math_quiz_web/components/math_components.ex
+++ lib/math_quiz_web/components/math_components.ex
@@ -2,7 +2,7 @@ defmodule MathQuizWeb.MathComponents do
   use Phoenix.Component
   use Gettext, backend: MathQuizWeb.Gettext
 
-  alias Phoenix.LiveView.JS
+  # alias Phoenix.LiveView.JS
   alias MathQuiz.Models
 
   attr :problem, :map
@@ -14,6 +14,19 @@ defmodule MathQuizWeb.MathComponents do
       <div>
         <div class="float-right">{@problem.second_num}</div>
         <div class="float-left">+</div>
+      </div>
+      <div class="py-2 my-2 border-t-2 self-start height-48px w-full"></div>
+    </div>
+    """
+  end
+
+  def math_problem(%{problem: %Models.MathQuizItem{operator: :subtract} = _problem} = assigns) do
+    ~H"""
+    <div class="p-4 flex flex-col text-xl items-end">
+      <div class="float-right">{@problem.first_num}</div>
+      <div>
+        <div class="float-right">{@problem.second_num}</div>
+        <div class="float-left">-</div>
       </div>
       <div class="py-2 my-2 border-t-2 self-start height-48px w-full"></div>
     </div>
